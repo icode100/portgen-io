5e6c8eb893ebbfe08af8f75034724f58edeb9308
asdfghkllzx <mahathibodela527@gmail.com>
2024-01-22 14:58:49 +0530

    setting page, home page, main page almost done css

---
+++ portgen-io/src/components/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style.css";
+import logo from "../assets/PortGenLogo.png";
+import rec from "../assets/rectangle.png"
+import hi from "../assets/hi-hello.gif";
+
+
+
+export default function HomePage () {
+  return (
+    <div className="home-page">
+        <div className="nav-bar">
+            <img id="portgen" src={logo} alt="logo" />
+            <p id="register">Register</p>
+        </div>
+        <div className="body">
+            <p id="headline-body-homePage">Create a stunning portfolio in minutes !</p>
+            <div className="footer-HomePage">
+                <img id ="hi" src={hi} alt="" />
+                <img id ="rec" src={rec} alt="back" /> 
+                
+            </div>
+        </div>
+    </div>
+  );
+};
---
+++ portgen-io/src/components/MainPage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Profile from "./MainPageProfile.jsx";
+import Logo from "../assets/PortGenLogo.png";
+import BackGround from "../assets/Vector 17.svg";
+import PortfolioCat from "./MainPgPortCatSet.jsx";
+import Prop1 from "../assets/Vector 23.svg";
+import Prop2 from "../assets/Vector 24.svg";
+import Prop3 from "../assets/Vector 25.svg";
+import Prop4 from "../assets/Vector 26.svg";
+import "../style.css";
+
+// data : [{title:
+//          images:}]
+
+export default function MainPage(props) {
+  let data = props.data;
+  let totalSets = [];
+  for(let i = 0; i < data.length; i++) {
+    totalSets.push(<PortfolioCat key={i} title={data[i].title} images={data[i].images} />);
+  }
+
+  return (
+    <div id="mainpage">
+
+      <div id ="mainpage-back">
+         <img  src={BackGround} alt="" />
+      </div>
+
+      <div id = "mainPageNavBar"> 
+        <img src={Logo} alt="" />
+        <Profile/>
+      </div>
+
+      {/* <div className="propsblueMainPage">
+        <img src={Prop1} alt="" id="prop1" class="propMain"/>
+        <img src={Prop2} alt="" id="prop2" class="propMain"/>
+        <img src={Prop3} alt="" id="prop3" class="propMain"/>
+        <img src={Prop4} alt="" id="prop4" class="propMain"/>
+      </div> */}
+      <div class = "here-mainpage">
+        <div id="mainPageBody">
+          {totalSets}    
+        </div>
+
+        <div className="mainPagefotter">
+          <p>Copyright © 2024 PortgenIo.Inc. All rights reserved.</p>
+        </div>
+      </div>
+    
+    </div>
+  
+  )
+}
