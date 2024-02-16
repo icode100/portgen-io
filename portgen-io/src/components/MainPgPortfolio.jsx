426325ff7f2528d26d813c72f2b7e426d3388fdd
icode100 <ipsit.das1@outlook.com>
2024-02-16 22:14:25 +0530

    resolution

--- portgen-io/src/components/MainPgPortfolio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MainPgPortfolio(props) {
-  return (
-    <div>
-        <img src={props.img} class ="mainPgPortfolio" alt="portofolio" />
-    </div>
-  )
-}
